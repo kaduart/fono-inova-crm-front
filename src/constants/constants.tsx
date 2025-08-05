4ea6faefcbd06e4cfb4847699bbc1e9fc9eee380
kadu art <ricardosantos.ti15@gmail.com>
2025-08-05 17:41:57 -0300

    cors

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,3 @@
-
 export const BASE_URL = import.meta.env.DEV
-  ? 'http://localhost:5000/api'  // Para desenvolvimento
-  : 'https://fono-inova-crm-back.onrender.com/api';  // Para produção
+  ? 'http://localhost:5000/api'
+  : '/api';
