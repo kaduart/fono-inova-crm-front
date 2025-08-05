c2e54b8625fa8e7c76e2855baa98b314840cc45b
kadu art <ricardosantos.ti15@gmail.com>
2025-08-05 17:38:58 -0300

    CORS POLICY

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,7 +1,4 @@
 
-// Para desenvolvimento
-/*  export const BASE_URL = import.meta.env.DEV
-  ? 'http://localhost:5000/api'   */
-
-// Para produção
-export const BASE_URL = 'https://fono-inova-crm-back.onrender.com/api'; 
+export const BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:5000/api'  // Para desenvolvimento
+  : 'https://fono-inova-crm-back.onrender.com/api';  // Para produção
