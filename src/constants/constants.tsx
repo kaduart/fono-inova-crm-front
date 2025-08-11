611b7d3d34d5508e190cd0c5842188de2e07d030
kadu art <ricardosantos.ti15@gmail.com>
2025-08-11 10:28:12 -0300

    teste comit

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,4 @@
 export const BASE_URL = import.meta.env.DEV
   //   ? 'http://localhost:8080/api'  // Desenvolvimento GO
   ? 'http://localhost:5000/api'  // Desenvolvimento NODE
-  : 'https://fono-inova-crm-back.onrender.com/api'; 
+  : 'https://fono-inova-crm-back.onrender.com/api';
